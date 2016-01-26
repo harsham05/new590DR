29f6f9da98e7310db7d6ac68270850757969280d
hm <harsham05@gmail.com>
2016-01-25 16:34:40 -0800

    working commit

--- src/fetch_all_solr_doc_ids.py
+++ src/fetch_all_solr_doc_ids.py
@@ -2,23 +2,38 @@
 #for large Solr Cores, use rows=0to50000
 
 from __future__ import print_function
-import json, requests, sys, os
+from pprint import pprint
+import json, requests, sys, os, argparse
 
 def main():
 
-    solrCoreURL = sys.argv[1]       # http://imagecat.dyndns.org/solr/dhsnewimagecatdev
-    solrCoreURL = solrCoreURL.split("://")[-1].rstrip('/')
+    parser = argparse.ArgumentParser(description="fetch all document ids efficiently from Solr index")
+    parser.add_argument('--solrURL', required=True, help="Solr Core URL")    
+    parser.add_argument('--outFile', required=True, help="text file containing doc IDs")
+    args = parser.parse_args()
 
-    lukeURL = "http://" + os.environ["SOLR_SIM_USER"]+":"+os.environ["SOLR_SIM_PASS"]+ "@" + solrCoreURL + "/admin/luke?fl=id&numTerms=1000&wt=json"
+    if args.solrURL and args.outFile:              # http://imagecat.dyndns.org/solr/dhsnewimagecatdev        
+        with open(args.outFile, 'w') as outF:
 
-    
+            lukeURL = "http://" + os.environ["SOLR_SIM_USER"]+":"+os.environ["SOLR_SIM_PASS"]+ "@" + args.solrURL.split("://")[-1].rstrip('/') + "/admin/luke?fl=id&numTerms=1000&wt=json"            
+            
+            try:
+                lukeResponse = requests.get(lukeURL, verify=False).json()
+            
+                topTerms = lukeResponse["fields"]["id"]["topTerms"]
 
-    try:
-        lukeResponse = requests.get(lukeURL)
-        pprint(lukeResponse)
+                i=0
+                while i < len(topTerms):
+                    print(topTerms[i], file=outF)
+                    i+=2
 
-    except:
-        print "Invalid Solr Core URL"
+                
+
+            except Exception as e:
+                print(e)
+            
+
+
 
 
 
