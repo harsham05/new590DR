156f77ba2dc6460c57a51737654a66589911de0a
hm <harsham05@gmail.com>
2016-02-04 15:19:08 -0800

    update bash job

--- src/first_half.sh
+++ src/first_half.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
-python solr_similarity.py --file chunks/doc_ids_0.txt --solrURL http://localhost:8983/solr/imagecatdev --type http --commit
-python solr_similarity.py --file chunks/doc_ids_500000.txt --solrURL http://localhost:8983/solr/imagecatdev --type http --commit
-python solr_similarity.py --file chunks/doc_ids_1000000.txt --solrURL http://localhost:8983/solr/imagecatdev --type http --commit
-python solr_similarity.py --file chunks/doc_ids_1500000.txt --solrURL http://localhost:8983/solr/imagecatdev --type http --commit
-python solr_similarity.py --file chunks/doc_ids_2000000.txt --solrURL http://localhost:8983/solr/imagecatdev --type http --commit  
-python solr_similarity.py --file chunks/doc_ids_2500000.txt --solrURL http://localhost:8983/solr/imagecatdev --type http --commit
-python solr_similarity.py --file chunks/doc_ids_3000000.txt --solrURL http://localhost:8983/solr/imagecatdev --type http --commit
+python solr_similarity.py --file chunks/doc_ids_0.txt --solrURL http://localhost:8983/solr/imagecatdev --type http --commit &
+python solr_similarity.py --file chunks/doc_ids_500000.txt --solrURL http://localhost:8983/solr/imagecatdev --type http --commit &
+python solr_similarity.py --file chunks/doc_ids_1000000.txt --solrURL http://localhost:8983/solr/imagecatdev --type http --commit &
+python solr_similarity.py --file chunks/doc_ids_1500000.txt --solrURL http://localhost:8983/solr/imagecatdev --type http --commit &
+sleep 10
+python solr_similarity.py --file chunks/doc_ids_2000000.txt --solrURL http://localhost:8983/solr/imagecatdev --type http --commit &
+python solr_similarity.py --file chunks/doc_ids_2500000.txt --solrURL http://localhost:8983/solr/imagecatdev --type http --commit &
+python solr_similarity.py --file chunks/doc_ids_3000000.txt --solrURL http://localhost:8983/solr/imagecatdev --type http --commit &
+python solr_similarity.py --file chunks/doc_ids_3500000.txt --solrURL http://localhost:8983/solr/imagecatdev --type http --commit &
+wait
+python solr_similarity.py --file chunks/doc_ids_4000000.txt --solrURL http://localhost:8983/solr/imagecatdev --type http --commit &
+python solr_similarity.py --file chunks/doc_ids_4500000.txt --solrURL http://localhost:8983/solr/imagecatdev --type http --commit &
+python solr_similarity.py --file chunks/doc_ids_5000000.txt --solrURL http://localhost:8983/solr/imagecatdev --type http --commit &
+sleep 10
+python solr_similarity.py --file chunks/doc_ids_5500000.txt --solrURL http://localhost:8983/solr/imagecatdev --type http --commit &
+python solr_similarity.py --file chunks/doc_ids_6000000.txt --solrURL http://localhost:8983/solr/imagecatdev --type http --commit &
+python solr_similarity.py --file chunks/doc_ids_6500000.txt --solrURL http://localhost:8983/solr/imagecatdev --type http --commit &
--- src/second_half.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-python solr_similarity.py --file chunks/doc_ids_3500000.txt --solrURL http://localhost:8983/solr/imagecatdev --type http --commit
-python solr_similarity.py --file chunks/doc_ids_4000000.txt --solrURL http://localhost:8983/solr/imagecatdev --type http --commit
-python solr_similarity.py --file chunks/doc_ids_4500000.txt --solrURL http://localhost:8983/solr/imagecatdev --type http --commit
-python solr_similarity.py --file chunks/doc_ids_5000000.txt --solrURL http://localhost:8983/solr/imagecatdev --type http --commit
-python solr_similarity.py --file chunks/doc_ids_5500000.txt --solrURL http://localhost:8983/solr/imagecatdev --type http --commit
-python solr_similarity.py --file chunks/doc_ids_6000000.txt --solrURL http://localhost:8983/solr/imagecatdev --type http --commit
-python solr_similarity.py --file chunks/doc_ids_6500000.txt --solrURL http://localhost:8983/solr/imagecatdev --type http --commit
